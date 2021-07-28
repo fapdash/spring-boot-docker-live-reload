92fc7adf1a818c0aa196b79984857c69ba7bf64b
fap <fapdash@users.noreply.github.com>
2021-07-28 16:31:26 +0200

    Initial commit

---
+++ src/main/java/com/example/livereload/SpringBootDockerLiveReloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.livereload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerLiveReloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerLiveReloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/livereload/SpringBootDockerLiveReloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livereload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerLiveReloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
